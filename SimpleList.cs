5ae9aae65616a131d1ff5733c8c58626cc86cb44
alberr22 <a.acebes.2021@alumnos.urjc.es>
2025-08-05 21:44:25 +0200

    pruebas

--- SimpleList.cs
+++ SimpleList.cs
@@ -1,22 +1,45 @@
-﻿using Unit;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-public class SimpleList<T> : ISimpleList
+// Asegúrate de que ISimpleList<T> esté definida correctamente en tu proyecto
+public class SimpleList<T> : ISimpleList<T>
 {
-	private List <T> lista = new List <T>
-	 
-	public SimpleList()
-	{
-	}
+    private List<T> lista = new List<T>();
 
-	public void AddElement ( T newElemnt){
+    public SimpleList()
+    {
+    }
 
-		lista.add(newElemnt);
+    // Implementa la propiedad de indexador
+    public T this[int index]
+    {
+        get => lista[index];
+        set => lista[index] = value;
+    }
 
-	}
+    // Implementa Count
+    public int Count => lista.Count;
 
-	public bool DeleteElement (T DeleteElement){
-		return lista.Remove(DeleteElement);
-	}
-	
+    // Implementa Add
+    public void Add(T item)
+    {
+        lista.Add(item);
+    }
+
+    // Implementa AddRange
+    public void AddRange(T[] collection)
+    {
+        lista.AddRange(collection);
+    }
+
+    // Implementa Remove
+    public bool Remove(T item)
+    {
+        return lista.Remove(item);
+    }
+
+    // Implementa Clear
+    public void Clear()
+    {
+        lista.Clear();
+    }
 }

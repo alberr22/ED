db6bd7b6a4ced257b83a8e00ea822f87893b7ae7
alberr22 <a.acebes.2021@alumnos.urjc.es>
2025-08-05 21:37:22 +0200

    Comit1

--- SimpleList.cs
+++ SimpleList.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 
 public class SimpleList<T> : ISimpleList
 {
+	private List <T> lista = new List <T>
+	 
 	public SimpleList()
 	{
 	}
+
+	public void AddElement ( T newElemnt){
+
+		lista.add(newElemnt);
+
+	}
+
+	public bool DeleteElement (T DeleteElement){
+		return lista.Remove(DeleteElement);
+	}
+	
 }
